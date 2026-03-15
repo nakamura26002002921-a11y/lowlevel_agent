65024b585ccf427810576d4a1ce6ca71c36c9078
nakamura26002002921-a11y <nakamura26002002921@gmail.com>
2026-03-15 17:28:12 +0900

    Update agent.py

--- install_python/agent.py
+++ install_python/agent.py
@@ -3,5 +3,5 @@ from pathlib import Path
 BASE_DIR = Path(__file__).parent
 def installpython(version, path):
     playbook_path = BASE_DIR / "install_python.yml"
-    cmd = ["ansible-playbook", str(playbook_path), "-e", f"VERSION={version} INSTALL_PATH={path}", "-c", "local"]
+    cmd = ["ansible-playbook", str(playbook_path), "-e", f"VERSION={version} INSTALL_PATH={path}", "-i", "localhost,", "-c", "local"]
     subprocess.run(cmd, check=True)
