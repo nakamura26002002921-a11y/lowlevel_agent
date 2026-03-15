1237e0531a6627c9aabe64f22d2fbc63adc2396a
中村真乃祐 <nakamuramashinnnosuke@Mac.lan>
2026-03-15 16:32:23 +0900

    Refactor agent.py and update functions

--- install_python/agent.py
+++ install_python/agent.py
@@ -3,11 +3,5 @@ from pathlib import Path
 BASE_DIR = Path(__file__).parent
 def installpython(version, path):
     playbook_path = BASE_DIR / "install_python.yml"
-    cmd = [
-        "ansible-playbook",
-        "-i", "hosts",
-        str(playbook_path),
-        "-e", f"VERSION={version} INSTALL_PATH={path}"
-    ]
-    print("[python_agent] Running:", " ".join(cmd))
+    cmd = ["ansible-playbook", str(playbook_path), "-e", f"VERSION={version} INSTALL_PATH={path}", "-c", "local"]
     subprocess.run(cmd, check=True)
